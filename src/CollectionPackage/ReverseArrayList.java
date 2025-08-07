a7e486bbbef67a9c4965d7520e3326772263aed2
kayasth1993 <kayasth1993@gmail.com>
2025-08-07 10:37:31 +0530

    new programs added in the list

--- src/CollectionPackage/ReverseArrayList.java
+++ src/CollectionPackage/ReverseArrayList.java
@@ -1,4 +1,22 @@
 package CollectionPackage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseArrayList {
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    List<Integer> list = new ArrayList<>();
+
+    for (int i : arr) {
+      list.add(i);
+    }
+
+    Collections.reverse(list);
+    System.out.println(list);
+
+
+  }
 }
